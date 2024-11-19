15e604104b9a6e68695e49dd7d10eea69f715b7d
Kay Ferraz <kayannesouza14@gmail.com>
2024-11-19 20:14:00 +0000

    começo aula 2

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import express from "express";
 
+const posts = [
+    {
+      descricao: "uma foto teste",
+        imagem: "https://placecats.com/millie/300/150",
+    },
+    { descricao: "Gato fazendo yoga", 
+        imagem: "https://placecats.com/millie/300/150"
+     },
+    {descricao: "Gato fazendo panqueca", 
+        imagem: "https://placecats.com/millie/300/150"}
+]
+
 const app = express();
 app.listen(3000, () => {
     console.log("servidor escutando");
@@ -8,3 +20,7 @@ app.listen(3000, () => {
 app.get("/api", (req ,res) => {
     res.status(200).send("rota inicial");
 });
+
+// erros 400>: erros na faixa do cliente
+// erros 500>: erros na faixa do servidor 
+// status http: https://http.cat/
