58e7ddfceecdd30bf7422fc76b2fda3c13a92a34
Kay Ferraz <kayannesouza14@gmail.com>
2024-11-20 20:08:43 +0000

    aula 3

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
 const posts = [
     { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
@@ -13,21 +15,29 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log("servidor escutando");
 });
-app.get("/posts", (req, res) => {
+
+async function getTodosPosts() {
+    const db = conexao.db("imersao-instabyte") 
+    const colecao = db.collection("posts") 
+    return colecao.find().toArray()
+}
+
+app.get("/posts",async (req, res) => {
+    const posts =  await getTodosPosts()
     res.status(200).json(posts);
 });
 
-function buscaPostPorId(id) {
-    return posts.findIndex((post)=> {
-            return post.id === Number (id)
-    })
-}
+// function buscaPostPorId(id) {
+//     return posts.findIndex((post)=> {
+//             return post.id === Number (id)
+//     })
+// }
 
-app.get("/posts/:id", (req ,res) => {
-    const index = buscaPostPorId(req.params.id)         //req tem valor de id
-    res.status(200).json(posts[index]);
-});
+// app.get("/posts/:id", (req ,res) => {
+//     const index = buscaPostPorId(req.params.id)         //req tem valor de id
+//     res.status(200).json(posts[index]);
+// });
 
-                                                        // erros 400>: erros na faixa do cliente
-                                                        // erros 500>: erros na faixa do servidor 
-                                                        // status http: https://http.cat/
+//                                                         // erros 400>: erros na faixa do cliente
+//                                                         // erros 500>: erros na faixa do servidor 
+//                                                         // status http: https://http.cat/
