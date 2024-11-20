2b5430866d174d62955e7200e9fd79c3e5a1a86c
Kay Ferraz <kayannesouza14@gmail.com>
2024-11-20 14:55:38 +0000

    aula 2

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
 import express from "express";
 
 const posts = [
-    {
-      descricao: "uma foto teste",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    { descricao: "Gato fazendo yoga", 
-        imagem: "https://placecats.com/millie/300/150"
-     },
-    {descricao: "Gato fazendo panqueca", 
-        imagem: "https://placecats.com/millie/300/150"}
-]
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+];
+
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("servidor escutando");
 });
-
-app.get("/api", (req ,res) => {
-    res.status(200).send("rota inicial");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
 
-// erros 400>: erros na faixa do cliente
-// erros 500>: erros na faixa do servidor 
-// status http: https://http.cat/
+function buscaPostPorId(id) {
+    return posts.findIndex((post)=> {
+            return post.id === Number (id)
+    })
+}
+
+app.get("/posts/:id", (req ,res) => {
+    const index = buscaPostPorId(req.params.id)         //req tem valor de id
+    res.status(200).json(posts[index]);
+});
+
+                                                        // erros 400>: erros na faixa do cliente
+                                                        // erros 500>: erros na faixa do servidor 
+                                                        // status http: https://http.cat/
