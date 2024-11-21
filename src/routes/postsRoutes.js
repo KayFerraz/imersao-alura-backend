e1c42e976932e3f8406fbd1fd51206865d4c4722
Kay Ferraz <kayannesouza14@gmail.com>
2024-11-21 20:00:20 +0000

    aula 4

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { listarPosts } from "../controller/postsController.js";
+import { listarPosts, postarNovoPost } from "../controller/postsController.js";
 
  const routes = (app) => {
     app.use(express.json());
-    app.get("/posts", listarPosts);
+    app.get("/posts", listarPosts);    //busca
+    app.post("/posts", postarNovoPost);//criar
 }
 export default routes;
 
