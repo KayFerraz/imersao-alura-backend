c7b37c8493b7923e1a597c2dc84228ee8132ae50
Kay Ferraz <kayannesouza14@gmail.com>
2024-11-23 15:56:29 +0000

    aula 4 finalizando

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,4 +1,5 @@
 import {getTodosPosts, criarPost} from "../models /postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
     const posts =  await getTodosPosts()
@@ -13,5 +14,23 @@ export async function postarNovoPost(req,res) {
     } catch (erro){
         console.error(erro.message);
         res.status(500).json ({"erro":"falha na requisição"})
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png` //juntar string com variavel pra criar uma string nova
+        fs.renameSync(req.file.path, imagemAtualizada) //fs = file sistem
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
     }
 }
